968eb5c1c6b939cb04929bd9bf225655af65cd32
Masozee <nurojilukmansyah@gmail.com>
2024-08-20 14:51:20 +0700

    update

--- core/settings.py
+++ core/settings.py
@@ -143,13 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # This is now both the source and destination
-
-# We don't need STATICFILES_DIRS if all files are already in STATIC_ROOT
-# STATICFILES_DIRS = []
+STATIC_ROOT = '/code/static'  # This matches the Docker mount point
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/code/media'  # Adjust if needed
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
