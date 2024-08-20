ed894c390ba497af1ab09f16aad0b25339e8ddc4
Masozee <nurojilukmansyah@gmail.com>
2024-08-20 12:35:51 +0700

    update

--- core/settings.py
+++ core/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(str(BASE_DIR), 'db.sqlite3'),
     }
 }
 # Password validation
